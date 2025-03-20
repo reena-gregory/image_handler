435a49e2dc2226fa764109032865c62f4d5b9173
Reena Gregory <reenagregory1993@gmail.com>
2025-03-20 16:51:36 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -2,6 +2,8 @@ import os
 
 AWS_REGION = "us-east-1"
 AWS_ENDPOINT_URL = "http://localhost:4566"
+AWS_ACCESS_KEY = "test"
+AWS_SECRET_KEY = "test"
 
 S3_BUCKET_NAME = "image-storage"
 DYNAMODB_DB_NAME = "dynamodb"
